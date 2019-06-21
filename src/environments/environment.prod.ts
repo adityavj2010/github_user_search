68d2c26abacaae47f11f777c83c09acf832f4bb0
Aditya Jagtap <adityajagtap@Adityas-MacBook-Air.local>
2019-06-22 00:44:25 +0530

    Changes to publish App on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+    //Enter GITHUB_USERNAME
+    GITHUB_USERNAME:"",
+    //Enter GITHUB_PASSWORD
+    GITHUB_PASSWORD:""  
 };
